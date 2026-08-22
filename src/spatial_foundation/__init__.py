0a8f6c0d292e8f04be06fd62b18043da55731171
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-22 20:02:09 -0300

    feat: expose GADM materialization at package boundary

--- src/spatial_foundation/__init__.py
+++ src/spatial_foundation/__init__.py
@@ -1,10 +1,13 @@
 from .catalog import DataRoot, register_external_snapshot, sha256_file
+from .geography import GADMMaterialization, materialize_gadm
 from .periods import Period, PeriodIndex
 
 __all__ = [
     "DataRoot",
+    "GADMMaterialization",
     "Period",
     "PeriodIndex",
+    "materialize_gadm",
     "register_external_snapshot",
     "sha256_file",
 ]
