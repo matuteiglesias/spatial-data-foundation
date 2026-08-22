c1c90f0ba06fffdc2039c57ecd2576702c981b97
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-22 20:36:06 -0300

    style: finish ruff import ordering

--- src/spatial_foundation/geography/gadm.py
+++ src/spatial_foundation/geography/gadm.py
@@ -2,8 +2,7 @@ from __future__ import annotations
 
 import geopandas as gpd
 
-from .models import geography_uid, GeometryRole
-
+from .models import GeometryRole, geography_uid
 
 REQUIRED_BASE = {"GID_0"}
 
