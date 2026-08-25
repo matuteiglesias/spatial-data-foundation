f191aff0825ff63706d39d2d920b8490654181a0
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-25 18:19:48 -0300

    style: normalize benchmark exports

--- benchmarks/__init__.py
+++ benchmarks/__init__.py
@@ -5,14 +5,20 @@ The benchmark package is development infrastructure, not part of the public
 """
 
 from .runner import CURRENT_ADAPTER, KernelAdapter, run_suite
-from .workloads import AREAL_CASES, POINT_CASES, PRESET_WIDTHS, make_areal_workload, make_point_workload
+from .workloads import (
+    AREAL_CASES,
+    POINT_CASES,
+    PRESET_WIDTHS,
+    make_areal_workload,
+    make_point_workload,
+)
 
 __all__ = [
     "AREAL_CASES",
     "CURRENT_ADAPTER",
-    "KernelAdapter",
     "POINT_CASES",
     "PRESET_WIDTHS",
+    "KernelAdapter",
     "make_areal_workload",
     "make_point_workload",
     "run_suite",
