979267cdfac861f14c25d1a22aa82a84819ce2a2
Matias Iglesias <matuteiglesias@gmail.com>
2026-08-25 17:05:16 -0300

    feat: expose optional presentation surface

---
+++ src/spatial_foundation/presentation/__init__.py
@@ -0,0 +1,10 @@
+from .basemaps import BASEMAP_ALIASES, BasemapSource, add_basemap, resolve_basemap
+from .plotting import plot_context
+
+__all__ = [
+    "BASEMAP_ALIASES",
+    "BasemapSource",
+    "add_basemap",
+    "plot_context",
+    "resolve_basemap",
+]
